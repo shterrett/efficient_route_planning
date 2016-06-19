cc217128bf89332eb25e546a815463f8e9638e80
Stuart Terrett <shterrett@gmail.com>
2016-06-19 07:51:14 -0400

    Contract full graph

--- src/contraction.rs
+++ src/contraction.rs
@@ -4,12 +4,20 @@ use std::collections::HashMap;
 use weighted_graph::{ Graph, Node };
 use pathfinder::{ CurrentBest, Pathfinder, EdgeIterator };
 
-pub fn local_shortest_path<'a, T>(graph: &'a Graph<T>,
-                                  source: &T,
-                                  destination: &T,
-                                  max_nodes: usize,
-                                  max_cost: i64
-                                 ) -> (i64, HashMap<T, CurrentBest<T>>)
+pub fn contract_graph<T>(graph: &mut Graph<T>,
+                         order: &Vec<T>)
+       where T: Clone + Eq + Hash {
+    for node in order {
+        contract_node(graph, &node);
+    }
+}
+
+fn local_shortest_path<'a, T>(graph: &'a Graph<T>,
+                              source: &T,
+                              destination: &T,
+                              max_nodes: usize,
+                              max_cost: i64
+                             ) -> (i64, HashMap<T, CurrentBest<T>>)
     where T: Clone + Hash + Eq {
     let identity = |_: Option<&Node<T>>, _ :Option<&Node<T>>| 0;
     let edge_iterator = |g: &'a Graph<T>, node_id: &T| ->
@@ -17,7 +25,7 @@ pub fn local_shortest_path<'a, T>(graph: &'a Graph<T>,
         Box::new(g.get_edges(node_id).iter().filter(|edge| edge.arc_flag))
     };
     let terminator = move |r: &CurrentBest<T>, rs: &HashMap<T, CurrentBest<T>>| {
-        rs.len() >= max_nodes || r.cost >= max_cost
+        rs.len() >= max_nodes || r.cost > max_cost
     };
     let pathfinder = Pathfinder::new(Box::new(identity),
                                      Box::new(edge_iterator),
@@ -26,8 +34,8 @@ pub fn local_shortest_path<'a, T>(graph: &'a Graph<T>,
     pathfinder.shortest_path(graph, source, Some(destination))
 }
 
-pub fn contract_node<T>(graph: &mut Graph<T>, node_id: &T)
-       where T: Clone + Eq + Hash {
+fn contract_node<T>(graph: &mut Graph<T>, node_id: &T)
+   where T: Clone + Eq + Hash {
     let adjacent_nodes = find_adjacent_nodes(graph, node_id);
 
     for adjacent in &adjacent_nodes {
@@ -60,6 +68,7 @@ fn find_adjacent_nodes<T>(graph: &Graph<T>, node_id: &T) -> Vec<T>
    where T: Clone + Hash + Eq {
     graph.get_edges(node_id)
          .iter()
+         .filter(|edge| edge.arc_flag)
          .map(|edge| edge.to_id.clone())
          .collect()
 }
@@ -95,14 +104,15 @@ fn add_shortcut<T>(graph: &mut Graph<T>, from_node: &T, to_node: &T, weight: i64
                    from_node.clone(),
                    to_node.clone(),
                    weight);
-    graph.get_mut_edge(from_node, to_node)
-         .map(|edge| edge.arc_flag = true);
 }
 
 #[cfg(test)]
 mod test {
     use weighted_graph::{ Graph };
-    use super::{ local_shortest_path, contract_node };
+    use super::{ local_shortest_path,
+                 contract_node,
+                 contract_graph
+               };
 
     #[test]
     fn local_shortest_path_terminates_early_by_cost() {
@@ -191,9 +201,9 @@ mod test {
                             .iter()
                             .find(|edge| edge.to_id == "a")
                             .unwrap();
-        assert!(added_ac.arc_flag);
+        assert!(!added_ac.arc_flag);
         assert_eq!(added_ac.weight, 2);
-        assert!(added_ca.arc_flag);
+        assert!(!added_ca.arc_flag);
         assert_eq!(added_ac.weight, 2);
 
         for edge in graph.get_edges(&"b") {
@@ -253,5 +263,68 @@ mod test {
                          .filter(|edge| edge.to_id == "b") {
             assert!(!edge.arc_flag);
         }
+    }
+
+    #[test]
+    fn contract_all_nodes() {
+        let mut graph = Graph::new();
+        let nodes = vec![("a", 0.0, 3.0),
+                         ("b", 0.0, 1.0),
+                         ("c", 0.0, 0.0),
+                         ("d", 1.0, 3.0),
+                         ("e", 1.0, 2.0),
+                         ("f", 1.0, 0.0),
+                         ("g", 2.0, 3.0),
+                         ("h", 2.0, 1.0),
+                         ("i", 2.0, 0.0)];
+        for &(id, x, y) in &nodes {
+            graph.add_node(id, x, y);
+        }
+
+        let edges = vec![("a", "b", 3),
+                         ("a", "d", 2),
+                         ("b", "c", 1),
+                         ("b", "e", 1),
+                         ("c", "f", 2),
+                         ("d", "e", 1),
+                         ("d", "g", 2),
+                         ("e", "f", 3),
+                         ("e", "h", 1),
+                         ("f", "i", 2),
+                         ("g", "h", 4),
+                         ("h", "i", 2),
+                        ];
+        for &(n1, n2, w) in &edges {
+            graph.add_edge(n1, n1, n2, w);
+            graph.add_edge(n2, n2, n1, w);
+            graph.get_mut_edge(&n1, &n2).map(|edge| edge.arc_flag = true);
+            graph.get_mut_edge(&n2, &n1).map(|edge| edge.arc_flag = true);
+        }
+
+        let shortcuts = vec![("c", "e", 2), ("e", "g", 3)];
+
+        let node_ids = nodes.iter()
+                            .map(|&(id, _, _)| id)
+                            .collect::<Vec<&str>>();
+        contract_graph(&mut graph, &node_ids);
+
+        for &(id, _, _) in &nodes {
+            assert!(graph.get_edges(&id).iter().all(|edge| !edge.arc_flag));
+        }
+        for &(n1, n2, w) in &shortcuts {
+            assert!(graph.get_edges(&n1)
+                         .iter()
+                         .find(|edge| edge.to_id == n2)
+                         .map(|edge| edge.weight == w).unwrap_or(false));
+            assert!(graph.get_edges(&n2)
+                         .iter()
+                         .find(|edge| edge.to_id == n1)
+                         .map(|edge| edge.weight == w).unwrap_or(false));
+        }
+
+        let edge_count = nodes.iter()
+                              .map(|&(id, _, _)| graph.get_edges(&id).len())
+                              .fold(0, |sum, l| sum + l);
+        assert_eq!(edge_count, shortcuts.len() * 2 + edges.len() * 2);
     }
 }
